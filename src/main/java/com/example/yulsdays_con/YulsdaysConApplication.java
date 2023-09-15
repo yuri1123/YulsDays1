102c9aa6e4620056a48918dd3f146c02fa7af412
Choi Yuri <yurifrom1123@gmail.com>
2023-09-15 11:07:53 +0900

    commit

---
+++ src/main/java/com/example/yulsdays_con/YulsdaysConApplication.java
@@ -0,0 +1,13 @@
+package com.example.yulsdays_con;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YulsdaysConApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YulsdaysConApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yulsdays_con/YulsdaysConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yulsdays_con;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YulsdaysConApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
